5d4a272dc0d8d77b9a3b6fa0f8de721e080da741
Joe Hawes <morehawes@gmail.com>
2026-01-20 09:34:26 -0800

    4

--- library/stores/geojson.js
+++ library/stores/geojson.js
@@ -1,20 +1,38 @@
-import { shallowRef } from "vue";
+import { shallowRef, onMounted, watch, computed } from "vue";
+import { throttle } from "lodash-es";
 import { defineStore } from "pinia";
 
 export const useGeoJSONStore = defineStore("geojson", () => {
 	// State
 	const geoJSON = shallowRef(null);
 
-	// Actions
-	function setGeoJSON(data) {
-		geoJSON.value = data;
-	}
+	// Persistence
+
+	const state = computed(() => geoJSON.value);
+
+	const toJSON = () => {
+		return geoJSON.value;
+	};
+
+	const fromJSON = (json) => {
+		geoJSON.value = json;
+	};
+
+	// Storage write
+	watch(
+		() => state.value,
+		throttle((newVal) => {
+			console.log("GeoJSON store changed:", newVal);
+		}, 1000),
+		{ deep: true },
+	);
 
 	return {
 		// State
-		geoJSON,
+		state,
 
-		// Actions
-		setGeoJSON,
+		// Persistence
+		toJSON,
+		fromJSON,
 	};
 });
